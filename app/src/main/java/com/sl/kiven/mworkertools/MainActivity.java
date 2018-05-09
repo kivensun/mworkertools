e5ba22021d0615000d06f5255d66e4a2529a4c1e
kivensun <kivensun@gmail.com>
2018-05-09 21:33:37 +0800

    add login and Main

--- app/src/main/java/com/sl/kiven/mworkertools/MainActivity.java
+++ app/src/main/java/com/sl/kiven/mworkertools/MainActivity.java
@@ -1,13 +1,47 @@
 package com.sl.kiven.mworkertools;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.about_me:
+                    mTextMessage.setText(R.string.about_me);
+                    return true;
+                case R.id.salary_list:
+                    mTextMessage.setText(R.string.salary_list);
+                    return true;
+                case R.id.notice_list:
+                    mTextMessage.setText(R.string.notice_list);
+                    return true;
+                case R.id.attendance_list:
+                    mTextMessage.setText(R.string.attendance_list);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
